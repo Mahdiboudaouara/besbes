84bb5d890165ed6ef18066657b76d37f564ec88a
maahdi boudaouara <mboudaouara@mercarue.com>
2022-08-01 16:29:14 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(()=>console.log("success"));
+  };
+
+  return (
+    <div className="App">
+      <div className="info">
+        <label>Name</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+        <label>Age</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        ></input>
+        <label>Country</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        ></input>
+        <label>Position</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        ></input>
+        <label>Wage</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        ></input>
+        <button onClick={addEmployee}>Add employee</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
